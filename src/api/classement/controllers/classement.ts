0e39f181a754efc181490be751ba723759ac2f27
Loik H <loik.hameaux@gmail.com>
2025-07-01 11:30:06 +0200

    Initial commit

---
+++ src/api/classement/controllers/classement.ts
@@ -0,0 +1,7 @@
+/**
+ * classement controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::classement.classement');
---
+++ src/api/classement/routes/classement.ts
@@ -0,0 +1,7 @@
+/**
+ * classement router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::classement.classement');
