09f70f0f47d67397754d206aed56aff19537a49d
Loik H <loik.hameaux@gmail.com>
2025-07-03 14:12:49 +0200

    Configure Strapi for production

---
+++ config/env/production/database.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    debug: false,
+  },
+});
