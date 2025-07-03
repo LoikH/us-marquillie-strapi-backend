fdc3bdd2c2f7a78cdb3d5af573883b7257ae8f52
Loik H <loik.hameaux@gmail.com>
2025-07-03 16:21:13 +0200

    Force postgres client in database config

--- config/database.js
+++ config/database.js
@@ -1,34 +1,25 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  if (env('NODE_ENV') === 'production') {
-    const { parse } = require('pg-connection-string');
-    const config = parse(env('DATABASE_URL'));
+  const client = env('DATABASE_CLIENT', 'sqlite');
 
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: { rejectUnauthorized: false }, // Nécessaire pour Render
-        },
-        debug: false,
-      },
-    };
-  }
-
-  // Configuration par défaut pour le développement (SQLite)
-  return {
-    connection: {
+  const connection = {
+    sqlite: {
       client: 'sqlite',
       connection: {
         filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
       useNullAsDefault: true,
     },
+    postgres: {
+      client: 'pg',
+      connection: env.url('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false },
+      debug: false,
+    },
+  };
+
+  return {
+    connection: connection[client],
   };
 };
